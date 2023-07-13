3f31e2ecee93f0e6cf1a13616dbd7f4bad09e34b
eghbal <eghbalshirasb@gmail.com>
2023-07-13 18:10:41 +0330

    add penalty calculation unit test

--- alidada/services/user_test.go
+++ alidada/services/user_test.go
@@ -2,6 +2,8 @@ package services
 
 import "testing"
 import "alidada/models"
+import "time"
+import "errors"
 
 type sortTest struct {
 	cancellationConditions       *[]models.CancellationCondition
@@ -57,5 +59,82 @@ func TestSort(t *testing.T) {
 				t.Errorf("Output %d not equal to expected %d test number %d", out.ID, test.sortedCancellationConditions[i].ID, j+1)
 			}
 		}
+	}
+}
+
+type penaltyTest struct {
+	reservation                  *models.Reservation
+	flightclass                  *models.FlightClass
+	sortedCancellationConditions []models.CancellationCondition
+	penalty                      int
+	error                        error
+}
+
+var penaltyTests = []penaltyTest{
+	penaltyTest{&models.Reservation{Price: 100},
+		&models.FlightClass{Flight: &models.Flight{StartTime: time.Now().Add(-1 * time.Minute * 41)}},
+		[]models.CancellationCondition{
+			{2, 40, "40 min", "", 10},
+			{3, 30, "30 min", "", 12},
+			{4, 20, "20 min", "", 70},
+			{1, 10, "10 min", "", 80},
+		},
+		10, nil,
+	},
+
+	penaltyTest{&models.Reservation{Price: 200},
+		&models.FlightClass{Flight: &models.Flight{StartTime: time.Now().Add(-1 * time.Minute * 34)}},
+		[]models.CancellationCondition{
+			{2, 40, "40 min", "", 10},
+			{3, 30, "30 min", "", 12},
+			{4, 20, "20 min", "", 70},
+			{1, 10, "10 min", "", 80},
+		},
+		24, nil,
+	},
+
+	penaltyTest{&models.Reservation{Price: 300},
+		&models.FlightClass{Flight: &models.Flight{StartTime: time.Now().Add(-1 * time.Minute * 24)}},
+		[]models.CancellationCondition{
+			{2, 40, "40 min", "", 10},
+			{3, 30, "30 min", "", 12},
+			{4, 20, "20 min", "", 70},
+			{1, 10, "10 min", "", 80},
+		},
+		210, nil,
+	},
+	penaltyTest{&models.Reservation{Price: 400},
+		&models.FlightClass{Flight: &models.Flight{StartTime: time.Now().Add(-1 * time.Minute * 12)}},
+		[]models.CancellationCondition{
+			{2, 40, "40 min", "", 10},
+			{3, 30, "30 min", "", 12},
+			{4, 20, "20 min", "", 70},
+			{1, 10, "10 min", "", 80},
+		},
+		320, nil,
+	},
+	penaltyTest{&models.Reservation{Price: 500},
+		&models.FlightClass{Flight: &models.Flight{StartTime: time.Now().Add(-1 * time.Minute * 8)}},
+		[]models.CancellationCondition{
+			{2, 40, "40 min", "", 10},
+			{3, 30, "30 min", "", 12},
+			{4, 20, "20 min", "", 70},
+			{1, 10, "10 min", "", 80},
+		},
+		100, errors.New("None of the cancellation conditions are available for you"),
+	},
+}
+
+func TestPenalty(t *testing.T) {
+	for j, test := range penaltyTests {
+		penalty, error := PenaltyCalculation(test.reservation, test.flightclass, test.sortedCancellationConditions)
+		if penalty != test.penalty {
+			t.Errorf("Output %d not equal to expected %d test number %d", penalty, test.penalty, j+1)
+		}
+		if (error != nil && test.error == nil) || (error == nil && test.error != nil) {
+			t.Errorf("Error not equal test number %d", j+1)
+
+		}
+
 	}
 }
