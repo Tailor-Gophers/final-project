abc9fd8e20c0231612bf660fe71d1b2c091d9a25
Amir Mohammad Valipour <ruasoftt@gmail.com>
2023-07-13 22:59:54 +0330

    change reserve of flight

--- mockapi/mockapi_data_sample.sql
+++ mockapi/mockapi_data_sample.sql
@@ -6,7 +6,7 @@ insert into flights values(2, null,null,null,'Shiraz', 'Tehran', '2020-02-08 15:
 
 insert into flights values(6, null, null, null,'Tehran', 'Alborz', '2020-02-13 09:30:41', '2020-02-13 10:00:41', 'mahan', 'Boeing737');
 
-insert into flight_classes values(4,null,null,null, 'Class-A', 1900, 50, 5,2);
+insert into flight_classes values(4,null,null,null, 'Class-A', 1900, 50, 15,2);
 
 insert into flight_classes values(1,null,null,null, 'Class-A', 1900, 50, 15,6);
 
